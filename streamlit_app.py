6a79e52ce116398b08279ae6befe0bcaa00cbed9
indiras2023 <151764146+indiras2023@users.noreply.github.com>
2023-11-22 11:21:45 -0500

    Update streamlit_app.py
    add birtirnoq forgotten before in the menu list text body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(Oatmeal & Milk)
-streamlit.text(Bluberry & Cream)
-streamlit.text(Cofee & Waffels)
+streamlit.text('Oatmeal & Milk')
+streamlit.text('Bluberry & Cream')
+streamlit.text('Cofee & Waffels')
