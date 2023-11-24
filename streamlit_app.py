5e73770a31f2b21f9575dff70125fae6c5c987d5
indiras2023 <151764146+indiras2023@users.noreply.github.com>
2023-11-24 18:20:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
-import streamlit
+gimport streamlit
 streamlit.title('My Moms New Healthy Dinner')
    
 streamlit.header('Breakfast favorites')
 streamlit.text('🥣Oatmeal & Milk')
 streamlit.text('🥗Bluberry & Brocoli')
-streamlit.text('🐔 Hard-Boiled Egg & Waffels')
+streamlit.text('🐔egg Hard-Boiled Egg & Waffels')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
